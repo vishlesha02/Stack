e26ee56751aa7ba35f35aef5cbfdd8ab970cff80
vishlesha02 <69750154+vishlesha02@users.noreply.github.com>
2023-05-08 20:33:15 +0530

    Add files via upload

--- Stack.java
+++ Stack.java
@@ -2,39 +2,47 @@ package Day10;
 
 import java.util.Scanner;
 public class Stack {
-        public int top=-1;
-        Scanner sc = new Scanner(System.in);
+    public int top=-1;
+    Scanner sc = new Scanner(System.in);
 
-        int size=sc.nextInt();
-        public int arr[]=new int[size];
-        public void push(int n){
-            if(top==size-1){
-                System.out.println("Stack is full");
-            }
-            else{
-                top+=1;
-                arr[top]=n;
-            }
+    int size=sc.nextInt();
+    public int arr[]=new int[size];
+    public void push(int n){
+        if(top==size-1){
+            System.out.println("Stack is full");
         }
-        public void pop(int n){
-            if(top==-1){
-                System.out.println("Stack is empty");
-            }
-            else{
-                top-=1;
-            }
-        }
-        public void show(){
-            for (int i=0;i<=top;i++){
-                System.out.println(arr[i]);
-            }
-        }
-        public static void main(String[] args) {
-            Stack st = new Stack();
-            st.push(70);
-            st.push(36);
-            st.push(56);
-            st.show();
+        else{
+            top+=1;
+            arr[top]=n;
         }
     }
+    public void pop(){
+        if(top==-1){
+            System.out.println("Stack is empty");
+        }
+        else{
+            top-=1;
+        }
+    }
+    public void peek(){
+        if(top!=-1){
+            System.out.println("top element is " + arr[top]);
+        }
+    }
+    public void show(){
+        for (int i=0;i<=top;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        st.push(70);
+        st.push(36);
+        st.push(56);
+        st.show();
+        st.pop();
+        st.show();
+        st.peek();
+    }
+}
 
